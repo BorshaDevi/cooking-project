30e42e8900c20c721c88412d9f4d92817c41d16f
Borshadevi <borshadevi27@gmail.com>
2024-03-24 14:51:48 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-2xl font-bold'>hello</h1>
+      <button className="btn">Button</button>
+    </>
+  )
+}
+
+export default App
