924d62c1ee54d22354b17d3c060adce47e0e92aa
Borshadevi <borshadevi27@gmail.com>
2024-03-24 15:11:15 +0600

    add text

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   
 
   return (
     <>
-      <h1 className='text-2xl font-bold'>hello</h1>
-      <button className="btn">Button</button>
+      <div>
+        <Navbar></Navbar>
+      </div>
     </>
   )
 }
