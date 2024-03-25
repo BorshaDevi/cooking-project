034e28f0020937a99897127bb7f56bc0e1e2291c
Borshadevi <borshadevi27@gmail.com>
2024-03-25 09:37:59 +0600

    add text

--- src/components/CookingMenu/Cooking.jsx
+++ src/components/CookingMenu/Cooking.jsx
@@ -14,7 +14,7 @@ const Cooking = ({cookName,count}) => {
         <p>Name<span className=' grid grid-cols-1'>{recipe_name}</span>  </p>
         <p> Time <span className=' grid grid-cols-1'>{preparing_time}</span> </p>
         <p> Calories <span className=' grid grid-cols-1'>{calories}</span>  </p>
-        <button className='text-black font-semibold btn bg-green-500 rounded-r-xl rounded-l-xl '>preparing</button>
+        <button className='text-black font-semibold btn bg-green-500 rounded-r-xl rounded-l-xl  p-4'>preparing</button>
     </div>
     <hr></hr>
     <h2 className="card-title">Currently cooking:</h2>
